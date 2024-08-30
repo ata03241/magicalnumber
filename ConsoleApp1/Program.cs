3f223f7ea51146f881b00f4ba50a27a8e96e7947
Ashok Tamang <pakhrintamangashok@gmail.com>
2024-08-30 10:54:13 +0200

    initial.commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace magicalnummer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program begin");
+
+            int sum = 0;
+
+            for (int i = 0; i <= 3; i++) {
+                sum += i; 
+
+                Console.WriteLine($"i = {i}");
+
+                if (i == 3) {
+                    Console.WriteLine("We have the magical sum " + i);
+                }
+            }
+            Console.WriteLine("Try again with different number ");
+            Console.ReadLine();
+            string again = "y";
+            
+
+            if (again == "y") {
+                int total = 0;
+
+                for (int i = 0; i <= 10; i++) {
+                    total += i; 
+
+                    Console.WriteLine($"i = {i}");
+
+                    if (i == 10) {
+                        Console.WriteLine("We have the magical sum " + i);
+                    }
+                }
+            } 
+            Console.WriteLine("program ends");
+            
+            
+        }
+    }
+}
